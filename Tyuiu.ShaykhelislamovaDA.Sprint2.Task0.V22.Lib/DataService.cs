eaeb3953888b6abe5bdb6f0f3e4099cda9d927f0
Диана Шайхелисламова <Tyuiu.ShaykhelislamovaDA@outlook.com>
2025-09-29 16:17:40 +0500

    Add project files.

---
+++ Tyuiu.ShaykhelislamovaDA.Sprint2.Task0.V22.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.ShaykhelislamovaDA.Sprint2.Task0.V22.Lib
+{
+    public class DataService : ISprint2Task0V22
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+            res[0] = x == y;
+            res[1] = x != y;
+            res[2] = x - 3029 < y;
+            res[3] = x > y;
+            res[4] = x - 3028 <= y;
+            res[5] = -1 * x >= y;
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ShaykhelislamovaDA.Sprint2.Task0.V22/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.ShaykhelislamovaDA.Sprint2.Task0.V22.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int x = 3105;
+        int y = 77;
+        bool[] res = new bool[6];
+        res = ds.GetCompareOperations(x, y);
+        Console.Title = "Спринт #2 | Выолнила: Шайхелисламова Д. А. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Операции сравнения                                                      *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #22                                                             *");
+        Console.WriteLine("* Выполнила: Шайхелисламова Диана Артуровна | ПКТБ-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+        Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических      *");
+        Console.WriteLine("* выражений, которая вернет логическую последовательность(массив):        *");
+        Console.WriteLine("* (False, True, True, True, True, False),)                                *"); 
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("x = 3105, y = 77");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        for (int i=0; i<6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+
+    }
+}

f3f440ec33ea80eade4445dece17cc5f5cf5cb9b
Диана Шайхелисламова <Tyuiu.ShaykhelislamovaDA@outlook.com>
2025-10-05 18:09:38 +0500

    Task6

--- Tyuiu.ShaykhelislamovaDA.Sprint2.Task6.V10.Lib/DataService.cs
+++ Tyuiu.ShaykhelislamovaDA.Sprint2.Task6.V10.Lib/DataService.cs
@@ -1,13 +1,33 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint2;
+﻿using System.Numerics;
+using tyuiu.cources.programming.interfaces.Sprint2;
 namespace Tyuiu.ShaykhelislamovaDA.Sprint2.Task6.V10.Lib
 {
     public class DataService : ISprint2Task6V10
     {
         public string FindDateOfPreviousDay(int g, int m, int n)
         {
-            switch ()
+            if (n == 1)
             {
-
+                switch (m)
+                {
+                    case 1: return $"{31}.{12}.{g-1}";
+                    case 2: return $"{31}.{m -1}.{g}";
+                    case 3: return $"{28}.{m -1}.{g}";
+                    case 4: return $"{31}.{m - 1}.{g}";
+                    case 5: return $"{30}.{m - 1}.{g}";
+                    case 6: return $"{31}.{m - 1}.{g}";
+                    case 7: return $"{30}.{m - 1}.{g}";
+                    case 8: return $"{31}.{m - 1}.{g}";
+                    case 9: return $"{31}.{m - 1}.{g}";
+                    case 10: return $"{30}.{m - 1}.{g}";
+                    case 11: return $"{31}.{m - 1}.{g}";
+                    case 12: return $"{30}.{m - 1}.{g}";
+                    default: throw new ArgumentException($"Месяц должен быть от 1 до 12. Значение {m}");
+                }
+            }
+            else
+            {
+                return $"{n - 1}.{m}.{g}";
             }
         }
     }
--- Tyuiu.ShaykhelislamovaDA.Sprint2.Task6.V10/Program.cs
+++ Tyuiu.ShaykhelislamovaDA.Sprint2.Task6.V10/Program.cs
@@ -1,9 +1,31 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint2;
-using Tyuiu.ShaykhelislamovaDA.Sprint2.Task6.V10.Lib;
+﻿using Tyuiu.ShaykhelislamovaDA.Sprint2.Task6.V10.Lib;
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выолнила: Шайхелисламова Д. А. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Получение результата из switch                                          *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #10                                                             *");
+        Console.WriteLine("* Выполнила: Шайхелисламова Диана Артуровна | ПКТБ-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+        Console.WriteLine("* оператора switch вычисляет требуемое значение и возвращает результат.   *");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите год:");
+        int g = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите месяц:");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите день:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.FindDateOfPreviousDay(g, m, n));
     }
 }
